6278b817496ba540a29b718ac52b40c1ea2c04da
root <root@i-00001fcc.(none)>
2013-01-09 13:43:09 +1100

    start

---
+++ site-cookbooks/cloudenabling/metadata.rb
@@ -0,0 +1,12 @@
+maintainer       "Ian Thomas"
+maintainer_email "ianedwardthomas@gmail.com"
+license          "All rights reserved"
+description      "Installs/Configures CloudEnabling"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.3"
+
+depends "build-essential"
+depends "git"
+depends "apt"
+depends "iptables"
+depends "postgresql"
